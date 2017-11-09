9983fd0fd06b2e57bcd4e308806b6ae6b81d6e40
Iolanta <16lyolya@gmail.ua>
2017-11-09 15:39:18 +0200

    111

--- server-app/app/models/company.js
+++ server-app/app/models/company.js
@@ -1,11 +1,11 @@
 let mongoose=require('mongoose');
 let Schema=mongoose.Schema;
 let companySchema=new Schema({
-    title:{type:String,required:title.length>10},
-    description:{type:String,required:description.length<1000},
-    image:{type:String,required},
-    image1:{type:String,required},
-	website:{type:string, required:}
+    title:{type:String,required:true, minlength:10},
+    description:{type:String,required:true, maxlength:1000},
+    image:{type:String,required:true},
+    image1:{type:String},
+	website:{type:string, required:true, pattern:"https?://....(?:\.[a-z].)"}
 	
 });
 module.exports=mongoose.model('company', companySchema);
